2cf4d5521d052fe25fffbf320cc5658d4425de02
Theo <theodonkulus@gmail.com>
2018-07-09 23:57:35 -0400

    Added servo defines so and proper init. Still needs work.
    Need to clean up.

--- servo_control_adafruit.ino
+++ servo_control_adafruit.ino
@@ -16,6 +16,7 @@
 //min -90  = 1.0 ms
 //neut 0   = 1.5 ms
 //max  90  = 2.0 ms
+//deadtime = 5us 
 //50 Hz
 
 //servo channel stuff
@@ -23,8 +24,34 @@
 #define SERVONEUT 305
 #define SERVOMAX  410 // this is the 'maximum' pulse length count (out of 4096)
 #define NUM_CHANNELS 16
-short ch[NUM_CHANNELS];
-short p_ch[NUM_CHANNELS];
+
+#define ANKLE_0  0
+#define HIP_0    1
+#define ANKLE_1  2
+#define HIP_1    3
+#define ANKLE_2  4
+#define HIP_2    5
+#define ANKLE_3  6
+#define HIP_3    7
+
+#define NUM_JOINTS 8
+
+typedef struct servo_s{
+  uint16_t curAngle; //8.8 -90 to 90 , with two sig digs.
+  uint16_t prevAngle; //8.8 -90 to 90 , with two sig digs.
+  uint16_t nextAngle; //8.8 -90 to 90 , with two sig digs.
+  uint16_t curTick; 
+  uint16_t prevTick;
+  uint16_t newTick;
+  unsigned char needsUpdate;
+  unsigned char pwmChannel; //channel for the PWM controller to modify  
+  unsigned char jointNum;//What joint in the chain it is
+} servo_t;
+
+static servo_t joints[NUM_JOINTS];
+
+unsigned short ch[NUM_CHANNELS];
+unsigned short p_ch[NUM_CHANNELS];
 
 //radio channel defines
 #define NUM_RADIO_CHAN 6
@@ -52,15 +79,24 @@ void setup()
 
     pwm.begin();
     pwm.setPWMFreq(50);  // Analog servos run at ~50 Hz updates
-    //set all channels to neutral position on startup
     
-    for(unsigned int i = 0; i < NUM_CHANNELS; i++)
+    //set all joint channels to neutral position on startup
+    for(unsigned int i = 0; i < NUM_JOINTS; i++)
     {
         ch[i] = 0;
         p_ch[i] = ch[i];
-        pwm.setPWM(i, 0, SERVONEUT);
+        pwm.setPWM(joints[i].PWMChannel, 0, SERVONEUT);
+        joints[i].curTick  = SERVONEUT;
+        joints[i].prevTick = SERVONEUT;
+        joints[i].newTick  = SERVONEUT;
+        joints[i].curAngle = 0;
+        joints[i].prevAngle = 0;
+        joints[i].nextAngle = 0;
+        joints[i].jointNum = i;
+        
     }
-
+    
+    //Setup reciever defines for each incomming signal
     pinMode(4, INPUT); //Elev
     pinMode(5, INPUT); //Rudd
     pinMode(6, INPUT); //Gear 
@@ -75,67 +111,91 @@ void loop()
     //update values based on input pulses from transmitter
     for(int i = 0; i < NUM_RADIO_CHAN; i++)
     {
-       rad_ch[i] = pulseIn(i+2, HIGH, 25000);
-       if( (abs(rad_ch[i] - prev_rad_ch[i]) > 10) && (rad_ch[i] != -1500) ) 
+       rad_ch[i] = pulseIn(i+2, HIGH, 20000);
+       if( (abs(rad_ch[i] - prev_rad_ch[i]) > 20)) 
        {
-           
-           Serial.print(frame, DEC);
+           /*Serial.print(frame, DEC);
            Serial.print(" Ch");
            Serial.print(i, DEC);
+           Serial.print(" idx");
+           Serial.print(i+2, DEC);
+           Serial.print(" ");
+           Serial.print(rad_ch[i]);
            Serial.print(" ");
            Serial.println(map(rad_ch[i], 1000,2000,-500,500)); // center at 0*/
                     
            diff = true;
        }    
     }    
+    delay(20);
     
     if(diff)
     {
-        if((val >= rad_ch[4]) && (toggle_switch_on == false))
+        /*if((val >= rad_ch[6]) && (toggle_switch_on == false))
         {
             toggle_switch_on = true;
             Serial.print("Serial mode engaged!");
         } else {
             toggle_switch_on = false;
-        }
-         ///Serial.println();
-         diff = false;
+        }*/
+         Serial.println();
 
 
-         int ch4_diff = rad_ch[4] -  prev_rad_ch[4];
-         if(ch4_diff > 0)
-         {
-            
-            for(int i = 0; i < NUM_CHANNELS; i++)
-            {
-                ch[i] += 1;
-                update_pwm = 1;
-                if(ch[i] >= SERVOMAX)
-                {
-                   update_pwm = 0;
-                   ch[i] = SERVOMAX;
-                }
-            }
-         } 
-         else if (ch4_diff < 0)
-         {
-            for(int i = 0; i < NUM_CHANNELS; i++)
-            {
-                ch[i] -= 1;
-                update_pwm = 1;
-                if(ch[i] < SERVOMIN)
-                {
-                   update_pwm = 0;
-                   ch[i] = SERVOMIN;
-                }
-            }
-         }
 
+           //int ch_diff = rad_ch[i] -  prev_rad_ch[i];
+           int ch_diff =  rad_ch[5] - prev_rad_ch[5];
+           int i = 15;
+           Serial.print("ch15 diff ");
+           Serial.print(ch_diff, DEC);
+           int gain = 2;
+           if( 
+           (abs(ch_diff) > 20))
+           {
+             
+             if(ch_diff > 1500)
+             {
+               
+                 ch[i] += gain*(rad_ch[5] / (SERVOMAX) - SERVOMIN);
+                 update_pwm = 1;
+                 if(ch[i] >= SERVOMAX)
+                 {
+                     update_pwm = 0;
+                     ch[i] = SERVOMAX;
+                 }
+                Serial.print(" UP ");   
+             } 
+             else if (ch_diff < 1500 )
+             {
+                 ch[i] -=  gain*(rad_ch[5] / (SERVOMAX) - SERVOMIN);
+                 update_pwm = 1;
+                 if(ch[i] < SERVOMIN)
+                 {
+                     update_pwm = 0;
+                     ch[i] = SERVOMIN;
+                 }
+                 Serial.print(" DOWN ");
+             } 
+           }
 
-         prev_rad_ch[4] = rad_ch[4];
+           prev_rad_ch[i] = rad_ch[i];
+           if(update_pwm)
+           {
+              Serial.print(ch[i], DEC);
+              Serial.print(" radio in ");
+              Serial.print(rad_ch[5]);
+              Serial.println();
+              if(ch[i] != p_ch[i])
+              {
+                  p_ch[i] = ch[i];
+                  pwm.setPWM(i, 0, ch[i]);
+              }
+          }
+    
+         update_pwm = 0;
+         diff = false;  
     }
 
-    if(toggle_switch_on == 1)
+    /*if(toggle_switch_on == 1)
     {
         //Read incomming serial commands then set PWM freq/params
         int bytes_available = Serial.available();
@@ -177,21 +237,8 @@ void loop()
             }
             Serial.println("16 channel Servo Aquire complete!");
         }
-    }
+    } */
 
-    if(update_pwm)
-    {
-        //update all the PWM channels
-        for(int i = 0; i < NUM_CHANNELS; i++)
-        {
-            if(ch[i] != p_ch[i])
-            {
-                p_ch[i] = ch[i];
-                pwm.setPWM(i, 0, ch[i]);
-            }
-        }
-    }
-    
-    update_pwm = 0;
+
     checksum = 0;
 }
